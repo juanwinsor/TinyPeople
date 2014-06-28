683734059068b5b22e4c071b9bc105276c97db60
juanwinsor <juan.winsor@gmail.com>
2014-06-28 19:38:13 -0400

    added crab assets
    added crab idle animation
    
    added left/right controls, hold down is constant velocity, tapping is increasing velocity

---
+++ Assets/Scripts/Crab/crabController.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crabController : MonoBehaviour {
+
+
+	private Animator animator;
+
+	const int maxVelocityAccumulator = 10;
+	int velocityAccumulatorLeft = 0;
+	int velocityAccumulatorRight = 0;
+
+	float velocity = 0;
+
+	float keyPressTimerPreviousLeft = 0;
+	float keyPressTimerCurrentLeft = 0;
+	float keyPressTimerPreviousRight = 0;
+	float keyPressTimerCurrentRight = 0;
+	float keyPressTimerInterval = 0.15f;
+
+	const int constantVelocity = 3;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//-- left key down
+		if( Input.GetKeyDown( KeyCode.A ) )
+		{
+			//-- check if the key press is a rapid succession
+			if( (keyPressTimerCurrentLeft - keyPressTimerPreviousLeft) < keyPressTimerInterval )
+			{
+				//-- it is a rapid key press so we increment the velocity accumulator
+				velocityAccumulatorLeft += 1;
+				//-- clamp the velocity accumulator to the max
+				Mathf.Min( velocityAccumulatorLeft, maxVelocityAccumulator ); 
+			}
+		}
+		//-- left key up
+		if( Input.GetKeyUp( KeyCode.A ) )
+		{
+			keyPressTimerPreviousLeft = keyPressTimerCurrentLeft;
+
+		}
+		keyPressTimerCurrentLeft = Time.time;
+		if( (keyPressTimerCurrentLeft - keyPressTimerPreviousLeft) > keyPressTimerInterval )
+		{
+			if( Input.GetKey( KeyCode.A ) )
+			{
+				velocityAccumulatorLeft = constantVelocity;
+			}
+			else
+			{
+				velocityAccumulatorLeft = 0;
+			}
+			
+		}
+
+
+		//-- right key down
+		if( Input.GetKeyDown( KeyCode.D ) )
+		{
+			//-- check if the key press is a rapid succession
+			if( (keyPressTimerCurrentRight - keyPressTimerPreviousRight) < keyPressTimerInterval )
+			{
+				//-- it is a rapid key press so we increment the velocity accumulator
+				velocityAccumulatorRight += 1;
+				//-- clamp the velocity accumulator to the max
+				Mathf.Min( velocityAccumulatorRight, maxVelocityAccumulator ); 
+			}
+		}
+		//-- left key up
+		if( Input.GetKeyUp( KeyCode.D ) )
+		{
+			keyPressTimerPreviousRight = keyPressTimerCurrentRight;
+			
+		}
+		keyPressTimerCurrentRight = Time.time;
+		if( (keyPressTimerCurrentRight - keyPressTimerPreviousRight) > keyPressTimerInterval )
+		{
+			if( Input.GetKey( KeyCode.D ) )
+			{
+				velocityAccumulatorRight = constantVelocity;
+			}
+			else
+			{
+				velocityAccumulatorRight = 0;
+			}
+		}
+
+
+
+		//-- calculate the left key velocity
+		float leftVelocity = velocityAccumulatorLeft * -0.5f;
+
+		//-- calculate the right key velocity
+		float rightVelocity = velocityAccumulatorRight * 0.5f;
+
+		//-- calculate the net velocity the player is inputting
+		velocity = leftVelocity + rightVelocity;
+
+		//-- set the velocity for the animation states
+		setVelocity( velocity );
+
+
+
+		Debug.Log( "velocity: " + velocity );
+
+
+		/*
+		//-- move left
+		if( Input.GetKey( KeyCode.D ) )
+		{
+			setVelocity( 1.0f );
+		}
+		else if( Input.GetKey( KeyCode.A ) )
+		{
+			setVelocity( -1.0f );
+		}
+		else
+		{
+			setVelocity( 0 );
+		}
+		*/
+	}
+
+	void setVelocity( float velocity )
+	{
+		if( animator )
+		{
+			animator.SetFloat( "velocity", velocity );
+		}
+	}
+}
