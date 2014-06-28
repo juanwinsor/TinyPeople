de008775024c9f22c4d960960c70cbb2cc50bab7
Tanner <tannerpyerichardson@gmail.com>
2014-06-28 16:10:49 -0400

    Working on Win Condition

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "net") 
+		{
+			//TODO: have a splash screen for winning
+			//Application.LoadLevel ("WaterJumperOver");
+			Debug.Log ("GOAL");
+		}
+	}
+}
